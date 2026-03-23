0844c0bf441801b58f65c9d23f354551f7dbc775
DeltaFlick12 <pedrolucasgomesbr1212@gmail.com>
2026-03-22 22:24:43 -0300

    model criado

---
+++ src/main/java/com/flick/projeto/model/Projeto.java
@@ -0,0 +1,59 @@
+package com.flick.projeto.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity(name = "Projeto")
+@Table(name = "tb_projeto")
+public class Projeto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_projeto")
+    private String nome;
+
+    @Column(name = "dat-inicio_projeto")
+    private LocalDate dataInicio;
+
+    private LocalDate dataFim;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+}
